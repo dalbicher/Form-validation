cd82b504c606ee5a72d07233302c83fc8d13752b
dalbicher <albicherd@gmail.com>
2021-05-03 19:12:25 +0200

    Form validation

---
+++ main.js
@@ -0,0 +1,30 @@
+const container = document.getElementById("page");
+const warningMessage = document.createElement('p');
+warningMessage.setAttribute('class', 'warning');
+warningMessage.textContent = "Paniek!";
+container.appendChild(warningMessage);$git
+
+const usernameField = document.getElementById("username");
+const warningMessageUsername = document.getElementById("username-warning");
+
+usernameField.addEventListener('keyup', getUsername);
+
+function getUsername(e) {
+    if(e.target.value.includes("@")) {
+        warningMessageUsername.textContent = "Gebruikersnaam mag geen @ bevatten";
+    } else {
+        warningMessageUsername.textContent = '';
+    }
+};
+
+const passwordField = document.getElementById("password");
+const warningMessagePassword = document.getElementById("password-warning");
+
+passwordField.addEventListener('keyup', getPassword);
+
+function getPassword(e) {
+    if(e.target.value === '') || e.target.value.length > 6) {
+    warningMessagePassword.textContent = '';
+} else (e.target.lenght < 6) {
+    warningMessagePassword = "Wachtwoord is te kort, gebruik minimaal 6 tekens";
+}
